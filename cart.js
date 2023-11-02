e07445f3bd4c812e6ea61ab6c409f905d038c0bb
Abhishek Singh <aksinghpro77@gmail.com>
2023-11-02 19:06:36 +0530

    Price Component in Cart Page

--- cart.js
+++ cart.js
@@ -15,7 +15,37 @@ cardContainer.addEventListener("click", (event)=>{
    cardContainer.innerHTML = "";
    createHorizontalProductCard(cart, cardContainer);
     localStorage.setItem("cart", JSON.stringify(cart));
+    location.reload();
 });
 //createProductCard(cart, cardContainer, findProductInCart, "cart");
 createHorizontalProductCard(cart, cardContainer);
+
+
+/**------------------Cart Total Section-------------------------- */
+const cartLength = document.querySelector(".item-count");
+
+//We are getting the value of cart from localstorage so that
+//if the cart is updated it would be updated in localstorage also
+//and the Price details also will be updated
+cartLength.innerText= JSON.parse(localStorage.getItem("cart")).length;
+//--------------------------------------------------------------------//
+
+
+//Price of Products---------------------------------------------//
+const productPrice = document.querySelector(".product-price");
+//console.log(productPrice);
+const priceAfterDiscount = JSON.parse(localStorage.getItem("cart")).reduce((acc, cur) => acc+cur.newPrice, 0);
+console.log(priceAfterDiscount);
+productPrice.innerText = priceAfterDiscount;
+
+const discount = document.querySelectorAll(".discount-amount");
+//console.log(discount);
+const priceBeforeDiscount = JSON.parse(localStorage.getItem("cart")).reduce((acc, cur) => acc+cur.oldPrice, 0);
+for (let element of discount){
+    element.innerText = priceBeforeDiscount - priceAfterDiscount;
+}
+
+const totalAmount = document.querySelector(".total-amount");
+totalAmount.innerText = priceAfterDiscount + 100;
+
 
